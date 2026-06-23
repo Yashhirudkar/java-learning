82a4dc5181876db1d73aa63f7b1ce17854ff94bc
Yash Hirudkar <yashhirudkar100@gmail.com>
2026-06-23 17:59:50 +0530

    2

--- basic of java/arrays.java
+++ basic of java/arrays.java
@@ -1,3 +1,82 @@
 
 // syntax of array
 //datatype[] arrayName = new datatype[size];
+//datatype arrayName[] = { , };
+
+// class DemoArray{
+//     public static void main(String[] args){
+//         int a[] = new int[5];
+//         a[0] = 5;
+//         a[1] = 2;
+//         a[2] = 10;
+//         a[3] = 8;
+//         a[4] = 3;
+
+//         for(int i = 0 ; i < a.length ; i++){
+//             System.out.println("Array element at index " + i + " is : " + a[i]);
+//         }
+//     }
+// }
+
+// class DemoArray{
+//  public static void main(String[] args){
+//     int[] a = {5,2,10,8,3};  
+
+//     for(int i = 0 ; i < a.length ; i++){
+//         System.out.println("Array element at index " + i + " is : " + a[i]);
+//     }
+//    }   
+// }
+
+//class DemoArray1{
+//    public static void main(String[] args){
+//        int sum = 0;
+//        int arr[] = {1,2,3,4,5};
+//
+//        for(int i = 0; i < arr.length; i++){
+//             sum = sum + arr[i];
+//        }
+//        System.out.println(sum);
+//    }
+//}
+
+//class arrays{
+//    public static void main(String[] args) {
+//        int[] arr = {5, 10, 15};
+//
+//        System.out.println(arr[1]);
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            System.out.println(arr[i]);
+//
+//        }
+//    }
+//}
+//
+
+//sum of numbers
+//class dsaarrys1{
+//    public static void main(String[] args){
+//        int[] arr = {10, 20, 30, 40};
+//        int sum = 0;
+//        for(int a = 0 ; a < arr.length; a++){
+//            sum = sum + arr[a];
+//        }
+//        System.out.println(sum);
+//    }
+//}
+
+//max no in array
+//class MaxNo{
+//    public static void main(String[] args){
+//        int[] arr = {5, 12, 3, 20, 8};
+//        int max =0;
+//        for(int i = 0; i <= arr.length - 1 ; i++){
+//            if(max < arr[i]){
+//             max = arr[i];
+//            }
+//
+//        }
+//        System.out.println(max);
+//    }
+//}
